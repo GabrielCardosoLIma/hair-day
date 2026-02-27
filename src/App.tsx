e79396ebdc40cf88aa74eb2dd5d129b077511b98
Gc20040702 <limag656@gmail.com>
2026-02-27 10:14:27 -0300

    feat: adiciona plugin svgr ao vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import { Text } from "./components/text";
+
 export function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div className="flex flex-col p-4 text-gray-100">
+      <div className="flex flex-col gap-2">
+        <Text variant="titleLg">Olá mundo!</Text>
+        <Text variant="titleMd">Olá mundo!</Text>
+        <Text variant="titleSm">Olá mundo!</Text>
+        <Text variant="textMd">Olá mundo!</Text>
+        <Text variant="textSm">Olá mundo!</Text>
+      </div>
+    </div>
+  );
 }
