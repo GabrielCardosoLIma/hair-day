f197be53ba567daad582312377e3e9090698885f
Gc20040702 <limag656@gmail.com>
2026-02-28 10:10:13 -0300

    feat: adiciona o componente ButtonIcon

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,45 @@
 import { Button } from "./components/button";
 import { Text } from "./components/text";
 import { TextInput } from "./components/text-input";
+import { ButtonIcon } from "./components/button-icon";
 
 import UserSquare from "./assets/icons/user-square.svg?react";
+import TrashIcon  from "./assets/icons/trash.svg?react";
 
 export function App() {
   return (
-    <div className="flex flex-col p-4 text-gray-100 space-y-4 max-w-80">
-      <div className="flex flex-col gap-2">
-        <Text variant="titleLg">Olá mundo!</Text>
-        <Text variant="titleMd">Olá mundo!</Text>
-        <Text variant="titleSm">Olá mundo!</Text>
-        <Text variant="textMd">Olá mundo!</Text>
-        <Text variant="textSm">Olá mundo!</Text>
-      </div>
+    <div className="mx-auto max-w-126 p-4">
+      <div className="grid gap-10">
+        <div className="flex flex-col gap-2">
+          <Text variant="titleLg" className="text-gray-100">
+            Olá mundo!
+          </Text>
+          <Text variant="titleMd" className="text-gray-100">
+            Olá mundo!
+          </Text>
+          <Text variant="titleSm" className="text-gray-100">
+            Olá mundo!
+          </Text>
+          <Text variant="textMd" className="text-gray-100">
+            Olá mundo!
+          </Text>
+          <Text variant="textSm" className="text-gray-100">
+            Olá mundo!
+          </Text>
+        </div>
 
-      <div className="flex flex-col gap-2">
-        <Button>Agendar</Button>
-        <Button disabled>Agendar</Button>
-      </div>
+        <div className="flex flex-col gap-2">
+          <Button>Agendar</Button>
+          <Button disabled>Agendar</Button>
+        </div>
 
-      <div className="flex flex-col gap-2">
-        <TextInput icon={UserSquare} placeholder="Nome do cliente" />
+        <div className="flex flex-col gap-2">
+          <TextInput icon={UserSquare} placeholder="Nome do cliente" />
+        </div>
+
+        <div>
+          <ButtonIcon icon={TrashIcon} />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/button-icon.tsx
@@ -0,0 +1,28 @@
+import type React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+import { Icon } from "./icon";
+
+export const buttonIconContainerVariants = cva(
+  `
+      inline-flex items-center justify-center cursor-pointer 
+      transition-colors group bg-transparent border-0 size-8
+    `
+);
+
+export const buttonIconIconVariants = cva(
+  "size-full fill-yellow group-hover:fill-yellow-dark transition-colors"
+);
+
+interface ButtonIconProps
+  extends React.ComponentProps<"button">,
+    VariantProps<typeof buttonIconContainerVariants> {
+  icon: React.ComponentProps<typeof Icon>["svg"];
+}
+
+export function ButtonIcon({ icon, className, ...props }: ButtonIconProps) {
+  return (
+    <button type="button" className={buttonIconContainerVariants({ className })} {...props}>
+      <Icon svg={icon} className={buttonIconIconVariants()} />
+    </button>
+  );
+}
